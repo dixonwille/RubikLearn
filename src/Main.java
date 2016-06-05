ec73ba14c103f49a49dec52b84d27d72f95f0b98
Will Dixon <dixonwille@hotmail.com>
2016-06-05 16:18:34 -0400

    Styled the look of the application

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,21 @@ import Cube.Canvas.CubeCanvas;
 import Cube.MoveType;
 import Cube.Cube;
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Stage window;
+    private Cube _cube;
+    private CubeCanvas _canvas;
 
     public static void main(String[] args) {
         launch(args);
@@ -24,48 +29,105 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Cube cube = new Cube();
+            _cube = new Cube();
+            _canvas = new CubeCanvas();
 
-            CubeCanvas canvas = new CubeCanvas();
             //Must first allow defaults to be set.
             //Then change any padding and sizes as need be.
             //Then update the canvases width and height using the getCanvasWidth and Height Methods
-            canvas.setWidth(canvas.getCanvasWidth());
-            canvas.setHeight(canvas.getCanvasHeight());
+            _canvas.setWidth(_canvas.getCanvasWidth());
+            _canvas.setHeight(_canvas.getCanvasHeight());
             //render the cube
-            canvas.render(cube);
+            _canvas.render(_cube);
 
+            //Place for all layouts to be handled
             BorderPane root = new BorderPane();
-            HBox hb = new HBox();
 
-            for(MoveType mt: MoveType.values()) {
-                Button btn = new Button(mt.toString());
-                btn.setOnAction(e -> {
-                    cube.move(mt);
-                    canvas.render(cube);
-                });
-                hb.getChildren().add(btn);
-            }
+            GridPane buttonLayout = createButtons();
 
-            Button resetBtn = new Button("Reset");
-            resetBtn.setOnAction(e -> {
-                cube.reset();
-                canvas.render(cube);
-            });
-            hb.getChildren().add(resetBtn);
-
-            hb.setAlignment(Pos.CENTER_LEFT);
-            root.setCenter(canvas);
-            root.setBottom(hb);
+            root.setCenter(_canvas);
+            root.setRight(buttonLayout);
             Scene scene = new Scene(root);
+            scene.getStylesheets().add("styles/rubikLearnStyle.css");
 
-            window = primaryStage;
-            window.setTitle("Rubik Learn");
-            window.setScene(scene);
-            window.show();
+            primaryStage.setTitle("Rubik Learn");
+            primaryStage.setScene(scene);
+            primaryStage.show();
         }catch (Exception e){
             e.printStackTrace();
             System.exit(1);
         }
+    }
+
+    private GridPane createButtons(){
+        //The Layout fot the buttons (kind of like a number pad on a phone)
+        GridPane buttonLayout = new GridPane();
+        buttonLayout.setVgap(20);
+        buttonLayout.setAlignment(Pos.CENTER);
+        buttonLayout.setPadding(new Insets(0,10,0,0)); //Same spacing as the canvas on other side.
+        //8 columns with the 3rd and 6th being spacers
+        for(int i =0; i < 8; i++){
+            ColumnConstraints column = new ColumnConstraints();
+            //Depending on the column in the group of 3 depends on the style.
+            switch (i%3){
+                case 0:
+                    column.setHalignment(HPos.RIGHT);
+                    break;
+                case 1:
+                    column.setHalignment(HPos.LEFT);
+                    break;
+                default: //2
+                    column.setPrefWidth(5);
+            }
+            buttonLayout.getColumnConstraints().add(column);
+        }
+
+        int[] position = new int[]{0,0};
+        //Make all the movement buttons
+        for(MoveType mt: MoveType.values()) {
+            //Create the button
+            //The way the enum is ordered allows me to know that button pairs are created sequentially
+            Button btn = new Button(mt.toString());
+            btn.setOnAction(e -> {
+                _cube.move(mt);
+                _canvas.render(_cube);
+            });
+            if(position[0]%3 == 0){
+                btn.getStyleClass().add("rightAlign");
+            }else{
+                btn.getStyleClass().add("leftAlign");
+            }
+
+            //Add button to grid.
+            buttonLayout.add(btn,position[0],position[1]);
+
+            //increment position
+            position[0]++;
+
+            //If we are in a spacing column skip and go to next
+            if(position[0]%3 == 2){
+                position[0]++;
+            }
+
+            //If we reached our max size move to the next row
+            if(position[0] >= 8){
+                position[0] = 0;
+                position[1]++;
+            }
+        }
+
+        Button resetBtn = new Button("Reset");
+        resetBtn.setOnAction(e -> {
+            _cube.reset();
+            _canvas.render(_cube);
+        });
+
+        //Have the reset button positioned in the center of the center two columns
+        HBox hb = new HBox();
+        hb.setAlignment(Pos.CENTER);
+        hb.getChildren().add(resetBtn);
+        buttonLayout.add(hb,3,position[1],2,1);
+
+        return buttonLayout;
     }
 }
